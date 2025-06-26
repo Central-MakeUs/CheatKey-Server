386a34af7e8243d51e8dfbf6a5e3b786c1acb0a8
Jisoo Han <hanjisoo93@gmail.com>
2025-06-26 12:43:29 +0900

    Initial commit

---
+++ src/main/java/com/cheatkey/CheatkeyApplication.java
@@ -0,0 +1,13 @@
+package com.cheatkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatkeyApplication.class, args);
+	}
+
+}
