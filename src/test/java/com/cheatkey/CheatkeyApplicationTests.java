6be50a587206c8f981c273ac0d4209d1789823ae
Jisoo Han <hanjisoo93@gmail.com>
2025-06-26 12:35:39 +0900

    Initial commit

---
+++ src/test/java/com/cheatkey/CheatkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheatkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
