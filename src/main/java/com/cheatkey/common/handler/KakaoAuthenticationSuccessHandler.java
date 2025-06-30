ed160ed1d7fb6e5ecf74ea585b358efbf2c2ca0c
Jisoo Han <hanjisoo93@gmail.com>
2025-06-30 19:26:32 +0900

    feat(config): OAuth2 기본 설정

---
+++ src/main/java/com/cheatkey/common/handler/KakaoAuthenticationSuccessHandler.java
@@ -0,0 +1,43 @@
+package com.cheatkey.common.handler;
+
+
+import com.cheatkey.module.auth.domain.entity.Auth;
+import com.cheatkey.module.auth.domain.repository.AuthRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class KakaoAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final AuthRepository authRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        Long kakaoId = user.getAttribute("kakaoId");
+
+        if (authRepository.findByKakaoId(kakaoId).isPresent()) {
+            Auth auth = authRepository.findByKakaoId(kakaoId).get();
+            auth.increaseLoginCount();
+            auth.updateLastLoginTime(LocalDateTime.now());
+            authRepository.save(auth);
+
+            // @TODO 신규 회원 표시하기
+            response.sendRedirect("/main");
+        } else {
+            response.sendRedirect("/auth/register?kakaoId=" + kakaoId);
+        }
+    }
+}
---
+++ src/main/java/com/cheatkey/common/sercurity/KakaoOAuth2UserService.java
@@ -0,0 +1,35 @@
+package com.cheatkey.common.sercurity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
+        OAuth2User oauth2User = delegate.loadUser(request);
+        Map<String, Object> attributes = oauth2User.getAttributes();
+
+        Long kakaoId = Long.parseLong(attributes.get("id").toString());
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                Map.of("kakaoId", kakaoId),
+                "kakaoId"
+        );
+    }
+}
