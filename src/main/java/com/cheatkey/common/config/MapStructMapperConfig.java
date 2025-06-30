ed2224d8a4d14994e332d298732716a4bc5ea953
Jisoo Han <hanjisoo93@gmail.com>
2025-06-30 19:27:16 +0900

    feat(config): JPA 기본 설정

---
+++ src/main/java/com/cheatkey/common/config/MapStructMapperConfig.java
@@ -0,0 +1,8 @@
+package com.cheatkey.common.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MapStructMapperConfig {
+}
