f05e599c1e0a171cf37678f1fa5d8fbecb3ccfc3
Jisoo Han <hanjisoo93@gmail.com>
2025-07-04 12:38:20 +0900

    feat: WebConfig에 CORS 설정 추가

--- src/main/java/com/cheatkey/common/config/web/WebConfig.java
+++ src/main/java/com/cheatkey/common/config/web/WebConfig.java
@@ -1,6 +1,7 @@
 package com.cheatkey.common.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,8 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/auth/login").setViewName("forward:/index.html");
-        registry.addViewController("/auth/register").setViewName("forward:/index.html");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://cheatkey.kr", "http://43.203.30.24")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
