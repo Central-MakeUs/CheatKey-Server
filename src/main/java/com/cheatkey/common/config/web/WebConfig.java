bc3fb9832e08861294c550ec7bbb5220c46dd8be
Jisoo Han <hanjisoo93@gmail.com>
2025-07-09 17:36:40 +0900

    chore: CORS 정책에 localhost 및 배포 서버 도메인 추가

--- src/main/java/com/cheatkey/common/config/web/WebConfig.java
+++ src/main/java/com/cheatkey/common/config/web/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "https://cheatkey.kr",
                         "http://43.203.30.24",
-                        "http://localhost:8080"
+                        "http://localhost:8080",    // Front-End
+                        "https://cheatcut.kro.kr"   // Front-End
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
