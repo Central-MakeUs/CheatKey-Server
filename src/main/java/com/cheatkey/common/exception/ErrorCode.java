bc805cde686bec9ac5cd177e7fa687404a8d88ab
Jisoo Han <hanjisoo93@gmail.com>
2025-07-18 11:51:16 +0900

    feat(exception): ErrorCode 추가

--- src/main/java/com/cheatkey/common/exception/ErrorCode.java
+++ src/main/java/com/cheatkey/common/exception/ErrorCode.java
@@ -32,11 +32,7 @@ public enum ErrorCode {
     INVALID_INPUT_TYPE_URL(HttpStatus.BAD_REQUEST, "검사 입력 타입은 URL이어야 합니다."),
     INVALID_INPUT_TYPE_CASE(HttpStatus.BAD_REQUEST, "검사 입력 타입은 CASE이어야 합니다."),
     DETECTION_FAILED(HttpStatus.BAD_REQUEST, "피싱 분석 중 오류가 발생 했습니다. 다시 시도해 주세요."),
-
-
-    /**
-     * 시스템 예외
-     */
+    GOOGLE_API_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "Google Safe Browsing API 호출에 실패했습니다. 나중에 다시 시도해주세요."),
 
     // JWT
     TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "헤더에 Access Token을 찾을 수 없습니다."),
@@ -45,15 +41,25 @@ public enum ErrorCode {
     MALFORMED_TOKEN(HttpStatus.BAD_REQUEST, "토큰 형식에 문제가 있습니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Refresh Token을 찾을 수 없습니다."),
 
+    INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "지원하지 않는 provider입니다."),
+
     // Cookie
     COOKIE_NOT_FOUND(HttpStatus.BAD_REQUEST, "요청에 쿠키가 존재하지 않습니다."),
 
+    // 파일 처리
+    FILE_EXTENSION_FAULT(HttpStatus.BAD_REQUEST, "해당 파일 확장자 명이 존재하지 않습니다."),
+
+
+    /**
+     * 시스템 예외
+     */
     // 404 Not Found
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
 
-    // 500 Internal Server Error
+    // 500 Internal Server Error,
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 처리 중 오류가 발생했습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/cheatkey/common/exception/ImageException.java
+++ src/main/java/com/cheatkey/common/exception/ImageException.java
@@ -1,6 +1,5 @@
 package com.cheatkey.common.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.IOException;
